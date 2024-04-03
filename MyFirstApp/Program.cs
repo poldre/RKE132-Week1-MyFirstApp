5388c92753be6d0072987cc9a74720cab3969b34
Tony. <antony.poldre@tktk.ee>
2024-04-03 21:00:00 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,16 @@
+﻿ // This is a comment to my code
+
+
+// Rakendus küsib kasutajal sisestada tema nime
+// Rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");
+//string - sõne
+string userName;
+
+userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + ", " + userName + "!");
+//string interpolation
+
+Console.WriteLine($"Hello, {userName} !");
